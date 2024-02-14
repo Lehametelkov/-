9b6d865bd0ea9c31b352dcdf1a1d759cfea08e49
Aleksey Metelkov <metelkov.aleksei@gmail.com>
2024-02-14 23:10:54 +0300

    гирьки

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 64;
+int b = 17;
+int c = 91;
+int d = 14;
+int e = 56;
+
+int max = a;
+
+if(b > max)
+{
+  max = b;  
+}
+if(c > max)
+{
+  max = c;  
+}
+if(d > max)
+{
+  max = d;  
+}
+if(e > max)
+{
+  max = e;  
+}
+Console.WriteLine(max);
