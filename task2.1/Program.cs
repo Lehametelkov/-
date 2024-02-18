db8dab9f11544fd4fa2bd9d1682e633cc7a864c5
Aleksey Metelkov <metelkov.aleksei@gmail.com>
2024-02-18 10:26:19 +0300

    Task2.1

---
+++ task2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа.
+//Примеры
+//a = 256=> 26
+//a = 891=> 81
+
+Console.WriteLine("Введите целое трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//int numChek = num;
+//if (num < 0) numCheck = num;
+
+if (num >=100 && num <= 999 || num >= -999 && num <= -100)
+{
+int firstDigit = num / 100; // 256/100 = 2.56
+int lastdigit = num % 10; // 256 % 10 = 250 + 6
+
+int result =firstDigit * 10 + lastdigit;
+Console.WriteLine($"{num} => {result}");
+}
