6dc88db1fa547eca1dbe04928d414e8d7c257b22
Aleksey Metelkov <metelkov.aleksei@gmail.com>
2024-02-21 18:45:39 +0300

    Делал дз

---
+++ proverka/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите целое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+      {
+        Console.WriteLine("первое число больше");
+      }
+      else if (firstNumber == secondNumber)
+      {
+        Console.WriteLine("числа равны"); 
+      }
+     
+      else if(firstNumber < secondNumber)
+      {
+        Console.WriteLine("второе число больше");        
+      }
+    
