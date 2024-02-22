89086a5c7ff4c402007dd5938183bc82097d9ba1
Aleksey Metelkov <metelkov.aleksei@gmail.com>
2024-02-22 16:29:14 +0300

    Сделал дз на кратность 7 и 23

---
+++ dz1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int remainder = num % 7 + num % 23;
+if (remainder == 0)
+{
+    Console.WriteLine($"{num} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"{num} Не кратно 7 и 23");
+}
