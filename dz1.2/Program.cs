6ff0de9c95c9c7ffd45a487276db44d855b2943a
Aleksey Metelkov <metelkov.aleksei@gmail.com>
2024-02-22 17:13:50 +0300

    задание про координаты

---
+++ dz1.2/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Напишите программу, которая принимает на вход координаты 
+//  точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+//  в которой находится эта точка.
+
+Console.WriteLine("Введите значение X");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Значение Y");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (Y != 0 && X != 0)
+{
+    if (Y > 0 && X > 0)
+    {
+        Console.WriteLine("Находится в первой четверти");
+    }
+    if (Y > 0 && X < 0)
+    {
+        Console.WriteLine("Находится во второй четверти");
+    }
+    if (Y < 0 && X < 0)
+    {
+        Console.WriteLine("Находится в третьей четверти");
+    }
+    if (Y < 0 && X > 0)
+    {
+        Console.WriteLine("Находится в четвертой четверти");
+    }
+}
+else
+{
+Console.WriteLine("Находится на оси координат");
+}
