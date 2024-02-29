c98ff3b7260346e1a1f10cd2d66fe830759b6d63
Aleksey Metelkov <metelkov.aleksei@gmail.com>
2024-02-29 17:00:20 +0300

    fun2

---
+++ lesson3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// возведение чисел от 1 до limit в квадрат с помощью функции
+
+
+void PrintSquares(int limit) // void потому что 
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.WriteLine($"{i*i} ");
+        i++;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
